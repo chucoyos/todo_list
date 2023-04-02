0f35d396e3b14d137ee6e5877ed9229a72cc4b69
unodev <unodev@outlook.com>
2023-04-02 16:27:17 -0600

    Remove comments

--- src/projects/projectsController.js
+++ src/projects/projectsController.js
@@ -2,10 +2,6 @@ function deleteProject(project) {
   const projects = JSON.parse(localStorage.getItem('projects'));
   const updatedProjects = projects.filter((proj) => proj.name !== project.name);
   localStorage.setItem('projects', JSON.stringify(updatedProjects));
-  // project.localStorage.removeItem("projects");
-  // const projects = JSON.parse(localStorage.getItem('projects'));
-  // const updatedProjects = projects.filter((proj) => proj.name !== project.name);
-  // localStorage.setItem('projects', JSON.stringify(updatedProjects));
 }
 
 export { deleteProject }
