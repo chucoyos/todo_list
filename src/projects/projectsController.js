f2a53fe63099ddea3553304b2be1061246a13de2
unodev <unodev@outlook.com>
2023-04-06 22:21:56 -0600

    Delete comments

--- src/projects/projectsController.js
+++ src/projects/projectsController.js
@@ -6,11 +6,6 @@ function deleteProject(project) {
 }
 
 function editProject(project) {
-  // const projects = JSON.parse(localStorage.getItem('projects'));
-  // const updatedProjects = projects.filter((proj) => proj.name !== project.name);
-  // localStorage.setItem('projects', JSON.stringify(updatedProjects));
-  // const addProjectButton = document.getElementById('add-project-button');
-
   // replace project title with input
   const editProjectInput = document.createElement('input');
   editProjectInput.classList.add('project-input');
