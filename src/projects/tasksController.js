5e93f83a5f90f68995a591a3882d996b0d7179ce
unodev <unodev@outlook.com>
2023-04-08 01:53:35 -0600

    Disable add task button after showing form

--- src/projects/tasksController.js
+++ src/projects/tasksController.js
@@ -2,6 +2,8 @@ import newTask from "./newTask";
 
 function showTaskForm() {
   newTask();
+  const addTaskButton = document.getElementById('add-project-button');
+  addTaskButton.disabled = true;
 }
 
 export { showTaskForm }
