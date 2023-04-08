90429a61a0e9d666a11b1f06421818c99dfea2ac
unodev <unodev@outlook.com>
2023-04-08 01:53:35 -0600

    Disable add task button after showing form

--- bundle.js
+++ bundle.js
@@ -196,7 +196,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \*****************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"showTaskForm\": () => (/* binding */ showTaskForm)\n/* harmony export */ });\n/* harmony import */ var _newTask__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./newTask */ \"./src/projects/newTask.js\");\n\n\nfunction showTaskForm() {\n  (0,_newTask__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n}\n\n\n\n//# sourceURL=webpack://todo-list/./src/projects/tasksController.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"showTaskForm\": () => (/* binding */ showTaskForm)\n/* harmony export */ });\n/* harmony import */ var _newTask__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./newTask */ \"./src/projects/newTask.js\");\n\n\nfunction showTaskForm() {\n  (0,_newTask__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n  const addTaskButton = document.getElementById('add-project-button');\n  addTaskButton.disabled = true;\n}\n\n\n\n//# sourceURL=webpack://todo-list/./src/projects/tasksController.js?");
 
 /***/ })
 
