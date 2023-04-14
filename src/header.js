d997f5ba503e688ba42e2b7cbcffe75accbd49a8
unodev <unodev@outlook.com>
2023-04-14 04:04:33 -0600

    Add app header

---
+++ src/header.js
@@ -0,0 +1,12 @@
+function header() {
+  const header = document.createElement('header');
+  header.classList.add('header');
+  const title = document.createElement('p');
+  title.textContent = 'This single page todo list app was built with webpack and javascript, uses local storage to store data. You can create multiple projects and each project can have a list of tasks with a due-date, a description and priority.';
+  header.appendChild(title);
+  const root = document.getElementById('root');
+  root.appendChild(header);
+  console.log('header');
+  return header;
+}
+export default header;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './styles/style.css';
 import projects from './projects/projects';
+import header from './header';
 
+header();
 projects();
